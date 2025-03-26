6f95a8f286ccb5fd0cf854cf1349dae07b6cfa7d
Jonguk, Yoon <89994165+YYoooonn@users.noreply.github.com>
2025-03-26 13:05:10 +0900

    fix: env variable usage with @Value

--- src/main/java/com/AIMLproject/backend/config/WebConfig.java
+++ src/main/java/com/AIMLproject/backend/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.AIMLproject.backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,7 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	private final String frontendOrigin = "${frontend.origin}";
+	@Value("${frontend.origin}")
+	private String frontendOrigin;
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
